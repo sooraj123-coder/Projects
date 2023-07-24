afa6054c449910cbfb3eaa180fd5f02e67f3946f
sooraj <soorajmailme@gmail.com>
2023-07-24 11:54:31 +0530

    Adding project

---
+++ shopsite/src/main/java/com/sooraj/Shop2Day/shopsite/ShopsiteApplication.java
@@ -0,0 +1,13 @@
+package com.sooraj.Shop2Day.shopsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsiteApplication.class, args);
+	}
+
+}
---
+++ shopsite/src/test/java/com/sooraj/Shop2Day/shopsite/ShopsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sooraj.Shop2Day.shopsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
